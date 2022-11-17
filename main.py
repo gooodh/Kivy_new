1379ffc144c479357a506db6abe1da458ac19009
Ser <s.b.nikuliny@gmail.com>
2022-11-17 20:54:48 +0700

    Паша ре..дюк

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-
 from kivy.core.window import Window
 from random import randint
 
